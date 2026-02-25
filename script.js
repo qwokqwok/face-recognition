4586eea384c29821978f99bf0a8258dd373116cd
qwokqwok <124016300+qwokqowk@users.noreply.github.com>
2026-02-25 12:46:45 +0700

    updaet path model

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let stream = null;
 let detectionInterval = null;
 let canvas = null;
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
 ]).then();
 
 async function startCamera() {
